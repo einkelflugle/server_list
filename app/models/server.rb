8808739c810494eff889b2d4db400c0916ddd915
einkelflugle <cluckyferret@gmail.com>
2014-12-29 14:03:37 +1000

    Improve layout and navigation

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,9 @@
 class Server < ActiveRecord::Base
 	belongs_to :user
+
+	validate :server_must_be_online
+
+	def server_must_be_online
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
     devise_for :users
 
     resources :servers
+    resources :users, only: [:show, :index]
     root 'servers#index'
 end
